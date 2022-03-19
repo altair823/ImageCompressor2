66bc8a49dd028c047731dbc932ab1f98e2ec4688
altair823 <rlaxogus823@naver.com>
2022-03-19 13:12:34 +0900

    remove console window in executable.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+#![windows_subsystem = "windows"]
+
 use eframe::{NativeOptions, run_native};
 use egui::Vec2;
 use ImageCompressor::App;
 
-
 fn main() {
-    #![windows_subsystem = "windows"]
     let app = App::default();
     let mut win_option = NativeOptions::default();
     win_option.initial_window_size = Some(Vec2::new(480., 795.));
